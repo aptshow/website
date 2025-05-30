5855097e398ffc043c068b5ae9c5656bb7ef63fb
Kyber <hipermig@gmail.com>
2025-05-30 11:56:31 +0200

    removed useless line

--- scroll-rotate.js
+++ scroll-rotate.js
@@ -2,6 +2,3 @@ window.addEventListener('scroll', () => {
     const rotation = window.scrollY / 10;
     document.body.style.transform = `rotate(${rotation}deg)`;
 });
-
-const audio = new Audio('music/aptheme.ogg');
-audio.play();
